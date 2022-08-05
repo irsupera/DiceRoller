fb6965697acf5f361aa86c4ccdd1bdb433cb8caa
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-05 01:38:46 -0400

    Create Die model class
    Create Die model class with 3 data fields (Name, NumberSides and CurrentSide) and 3 constructors having 0, 1 (numberSides) and 2 (namePrefix and numberSides) arguments.

---
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace DiceRoller.Models
+{
+    public class Die
+    {
+        public string Name { get; set; }
+        public int NumberSides { get; set; }
+        public int CurrentSide { get; set; }
+
+        public Die()
+        {
+            Name = "d6";
+            NumberSides = 6;
+            Roll();
+        }
+
+        public Die(int numberSides)
+        {
+            Name = "d" + numberSides;
+            NumberSides = numberSides;
+            Roll();
+        }
+
+        public Die(string namePrefix, int numberSides)
+        {
+            Name = namePrefix + numberSides;
+            NumberSides = numberSides;
+            Roll();
+        }
+
+    }
+}
+
