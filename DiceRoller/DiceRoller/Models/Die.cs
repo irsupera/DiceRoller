eeb7ffc53e0ce1a6cb4746d3d9bf663d2c28096f
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-10 22:51:37 -0400

    Create unit test for constructor with parameters and get/set methods
    Create unit test for constructor with 1 parameter (numberSides) and 2 parameters (namePrefix and numberSides).
    Add test for get/set methods, to set side up and check current side up is valid, and get default & custom name.

--- DiceRoller/DiceRoller/Models/Die.cs
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -7,34 +7,34 @@ namespace DiceRoller.Models
         public int NumberSides { get; set; }
         public int CurrentSide { get; set; }
 
-        public Die()
+        public Die() // default constructor 0 argument
         {
             Name = "d6";
             NumberSides = 6;
             Roll();
         }
 
-        public Die(int numberSides)
+        public Die(int numberSides) // constructor 1 argument
         {
             Name = "d" + numberSides;
             NumberSides = numberSides;
             Roll();
         }
 
-        public Die(string namePrefix, int numberSides)
+        public Die(string namePrefix, int numberSides) // constructor with 2 arguments
         {
             Name = namePrefix + numberSides;
             NumberSides = numberSides;
             Roll();
         }
 
-        public void Roll()
+        public void Roll() // set current side - random number from 1 to no. of sides
         {
             Random r = new Random();
             CurrentSide = r.Next(NumberSides)+1;
         }
 
-        public void SetSideUp(int newSideUp)
+        public void SetSideUp(int newSideUp) // set current side up from argument
         {
             if(newSideUp >= 1 && newSideUp <= NumberSides)
             {
--- DiceRollerTest/DieUnitTest.cs
+++ DiceRollerTest/DieUnitTest.cs
@@ -33,6 +33,88 @@ namespace DiceRollerTest
             }
         }
 
+        [TestMethod]
+        [DataRow(3, "d3")]
+        [DataRow(4, "d4")]
+        [DataRow(8, "d8")]
+        public void DieHasCustomSides(int sides, string name)
+        {
+            Die d1 = new Die(sides); 
+            d1.Name.Should().Be(name);
+            d1.NumberSides.Should().Be(sides);
+            d1.CurrentSide.Should().BeInRange(1, sides);
+        }
+
+        [TestMethod]
+        [DataRow(3, "x", "x3")]
+        [DataRow(4, "x", "x4")]
+        [DataRow(8, "x", "x8")]
+        public void DieHasCustomNames(int sides, string prefix, string name)
+        {
+            Die d1 = new Die(prefix, sides);
+            d1.Name.Should().Be(name);
+            d1.NumberSides.Should().Be(sides);
+            d1.CurrentSide.Should().BeInRange(1, sides);
+        }
+
+        [TestMethod]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(8)]
+        public void DefaultRollSetsSideCorrectlyCustomSides(int sides)
+        {
+            Die d1 = new Die(sides);
+            for (int i = 0; i < 100; i++)
+            {
+                d1.Roll();
+                d1.CurrentSide.Should().BeInRange(1, sides);
+            }
+        }
+
+        //[TestMethod]
+        //[DataRow(3, 2)]
+        //[DataRow(4, 2)]
+        //public void SetSideUpChangesSide(int sides, int newSides)
+        //{
+        //    Die d1 = new Die(sides);
+        //    //d1.SetSide(newSides); 
+        //    //d1.CurrentSide.Should().Be(sides);
+        //}
+
+        [TestMethod]
+        [TestCategory("SetMethods")]
+        [DataRow(3, 2)]
+        [DataRow(5, 10)]
+        [DataRow(10, -12)]
+        public void SetSideUpSetsValidSide(int sides, int newSide)
+        {
+            Die d1 = new Die(sides);
+            d1.SetSideUp(newSide);
+            d1.GetCurrentSide().Should().BeInRange(1, sides);
+            if (newSide >= 1 && newSide <= sides)
+                d1.GetCurrentSide().Should().Be(newSide);
+        }
+
+        [TestMethod]
+        [TestCategory("GetMethods")]
+        public void GetDefaultNameReturnsValue()
+        {
+            d.GetName().Should().BeOfType<string>();
+            d.GetName().Should().Be("d6");
+        }
+
+        [TestMethod]
+        [TestCategory("GetMethods")]
+        [DataRow(3, "x", "x3")]
+        [DataRow(4, "x", "x4")]
+        [DataRow(8, "x", "x8")]
+        public void GetCustomNameReturnsValue(int sides, string prefix, string name)
+        {
+            Die d1 = new Die(prefix, sides);
+            d1.GetName().Should().BeOfType<string>();
+            d1.GetName().Should().Be(name);
+        }
+
     }
 }
 
